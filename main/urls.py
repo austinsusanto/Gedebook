e84c1447e908a45c4f283b31c5a5f90b621e7162
Austin <austinsusanto@gmail.com>
2023-09-26 21:19:24 +0700

    Update: Finished Tugas 4

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, delete_product, add_product_by_one, reduce_product_by_one
 
 app_name = 'main'
 
@@ -10,4 +10,12 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('delete-product/<int:id>/', delete_product, name='delete_product'),
+    path('add-product-by-one/<int:id>/',
+         add_product_by_one, name='add_product_by_one'),
+    path('reduce-product-by-one/<int:id>',
+         reduce_product_by_one, name='reduce_product_by_one')
 ]
